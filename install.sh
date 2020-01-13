2b947c1a63b00abf0909910757d54d9be171f9f0
Hữu Hà ZSmart <business@zsmart.tech>
2020-01-14 01:48:47 +0700

    Update and rename install to install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,15 @@ echo "Start install docker-compose"
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "Docker-compose install done"
 echo "Start install docker-machine"
-
-sudo chmod +x /usr/local/bin/docker-compose
 
 curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && \
 chmod +x /tmp/docker-machine && \
 sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
+echo "Docker-machine install done"
+
+echo "All work are done :) have fun!"
